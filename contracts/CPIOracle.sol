c13f393a7a34b0e5a94ea67eed346ef701d5184d
rebasedev <rebasedev@criptext.com>
2020-11-13 18:05:12 -0500

    create Oracle cpi

---
+++ contracts/CPIOracle.sol
@@ -0,0 +1,58 @@
+pragma solidity 0.4.24;
+
+import "openzeppelin-eth/contracts/math/SafeMath.sol";
+import "openzeppelin-eth/contracts/ownership/Ownable.sol";
+
+import "./lib/SafeMathInt.sol";
+import "./lib/UInt256Lib.sol";
+import "./Rebase.sol";
+
+
+
+interface IOracle {
+    function getData() external returns (uint256, bool);
+}
+
+
+contract MarketOracle is Ownable {
+    using SafeMath for uint256;
+    using SafeMathInt for int256;
+    using UInt256Lib for uint256;
+
+    event LogCPI(
+        uint256 indexed cpi,
+        bool validity
+    );
+
+    // cpi value, in order to get target =1, cpi and baseCpi must have the same value
+    uint256 public cpi = 0;
+
+    uint256 private constant DECIMALS = 18;
+
+
+    function initialize(address owner_)
+    public
+    initializer
+    {
+        Ownable.initialize(owner_);
+    }
+
+
+    function getData()
+    external
+    returns (uint256, bool)
+    {
+        bool validity = cpi > 0 ;
+        emit LogCPI(cpi,validity);
+        return (cpi, validity);
+    }
+
+
+    function setCPIValue(uint256 data)
+    public
+    onlyOwner
+    {
+        cpi = data;
+    }
+
+}
